fdc172bda949949c7c222385545adb15ed56129f
m-olfatee <m.olfatee@gmail.com>
2020-04-15 14:14:31 +0200

    wed 14:14

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
-var curent = 1;
 function right(i) {
-    console.log(i)
     var linkBox = document.getElementsByClassName("section-first-link");
-    console.log(linkBox.item(i - 1))
-    linkBox.item(i - 2).style.display = "none";
-    linkBox.item(i - 1).style.display = "block";
-    alert(document.getElementById("index-section-first").style.backgroundImage);
-    document.getElementById("index-section-first").style.backgroundImage = "url('../img/menu2.png')";
-
-    //document.getElementById("index-section-first").style.backgroundImage = "url('../img/menu_2.png')";
+    if (i < 6 && i != 1) {
+        linkBox.item(i - 2).style.display = "none";
+        linkBox.item(i - 1).style.display = "block";
+        document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    } else if (i == 1) {
+        linkBox.item(4).style.display = "none";
+        linkBox.item(i - 1).style.display = "block";
+        document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    }
+    if (i % 2 == 0) {
+        document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
+        document.getElementById("contact-link").style.color = "#000"
+    } else {
+        document.getElementById("logo").src = "/assets/img/logo_hell.png";
+        document.getElementById("contact-link").style.color = "#fafafa"
+    }
+}
+function left(i) {
+    var linkBox = document.getElementsByClassName("section-first-link");
+    if (i > 0 && i != 5) {
+        linkBox.item(i).style.display = "none";
+        linkBox.item(i - 1).style.display = "block";
+        document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    } else if (i == 5) {
+        linkBox.item(0).style.display = "none";
+        linkBox.item(4).style.display = "block";
+        document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    }
 }
