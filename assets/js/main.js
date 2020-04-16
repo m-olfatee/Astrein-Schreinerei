f12801318cc60fbbbc111c5afb8a99200e7deb35
m-olfatee <m.olfatee@gmail.com>
2020-04-16 15:47:58 +0200

    thu 1547

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,69 @@
-// function right(i) {
-//     var linkBox = document.getElementsByClassName("section-first-link");
-//     if (i < 6 && i != 1) {
-//         linkBox.item(i - 2).style.display = "none";
-//         linkBox.item(i - 1).style.display = "block";
-//         document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-//     } else if (i == 1) {
-//         linkBox.item(4).style.display = "none";
-//         linkBox.item(i - 1).style.display = "block";
-//         document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-//     }
-//     if (i % 2 == 0) {
-//         document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
-//         document.getElementById("contact-link").style.color = "#000"
-//     } else {
-//         document.getElementById("logo").src = "/assets/img/logo_hell.png";
-//         document.getElementById("contact-link").style.color = "#fafafa"
-//     }
-// }
-// function left(i) {
-//     var linkBox = document.getElementsByClassName("section-first-link");
-//     if (i > 0 && i != 5) {
-//         linkBox.item(i).style.display = "none";
-//         linkBox.item(i - 1).style.display = "block";
-//         document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-//     } else if (i == 5) {
-//         linkBox.item(0).style.display = "none";
-//         linkBox.item(4).style.display = "block";
-//         document.getElementById("index-section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-//     }
-// }
+function right(i) {
+    var linkBox = document.getElementsByClassName("section-first-link");
+    if (i < 6 && i != 1) {
+        linkBox.item(i - 2).style.display = "none";
+        linkBox.item(i - 1).style.display = "block";
+        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    } else if (i == 1) {
+        linkBox.item(4).style.display = "none";
+        linkBox.item(i - 1).style.display = "block";
+        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    }
+    if (i % 2 == 0) {
+        document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
+        document.getElementById("contact-link").style.color = "#000";
+        for (let j = 0; j < 5; j++) {
+            document.getElementsByClassName("hover")[j].style.color = "#000";
+        }
+
+    } else {
+        document.getElementById("logo").src = "/assets/img/logo_hell.png";
+        document.getElementById("contact-link").style.color = "#fafafa";
+        for (let j = 0; j < 5; j++) {
+            document.getElementsByClassName("hover")[j].style.color = "#fafafa";
+        }
+    }
+}
+function left(i) {
+    var linkBox = document.getElementsByClassName("section-first-link");
+    if (i > 0 && i != 5) {
+        linkBox.item(i).style.display = "none";
+        linkBox.item(i - 1).style.display = "block";
+        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    } else if (i == 5) {
+        linkBox.item(0).style.display = "none";
+        linkBox.item(4).style.display = "block";
+        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+    }
+    if (i % 2 == 0) {
+        document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
+        document.getElementById("contact-link").style.color = "#000";
+        for (let j = 0; j < 5; j++) {
+            document.getElementsByClassName("hover")[j].style.color = "#000";
+        }
+    } else {
+        document.getElementById("logo").src = "/assets/img/logo_hell.png";
+        document.getElementById("contact-link").style.color = "#fafafa";
+        for (let j = 0; j < 5; j++) {
+            document.getElementsByClassName("hover")[j].style.color = "#fafafa";
+        }
+    }
+}
+function grid(i) {
+    var linkBox = document.getElementsByClassName("section-first-link");
+    if (i == 0) {
+
+        linkBox.item(i).style.display = "none";
+
+        document.getElementById("section-first").style.backgroundImage = "url('')";
+        document.getElementById("grid").style.display = "grid";
+        document.getElementById("logo").src = "/assets/img/logo_astrein.svg";
+        document.getElementById("contact-link").style.color = "#000";
+        for (let j = 0; j < 5; j++) {
+            document.getElementsByClassName("hover")[j].style.color = "#000";
+        }
+    }
+}
 
 
 
