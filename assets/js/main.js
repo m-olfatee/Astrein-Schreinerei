cea526a217364ac41c8494c1d90954effc63d955
m-olfatee <m.olfatee@gmail.com>
2020-04-21 01:18:24 +0200

    tue 0118

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,54 @@
-function right(i) {
-    var linkBox = document.getElementsByClassName("section-first-link");
-    if (i < 6 && i != 1) {
-        linkBox.item(i - 2).style.display = "none";
-        linkBox.item(i - 1).style.display = "block";
-        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-    } else if (i == 1) {
-        linkBox.item(4).style.display = "none";
-        linkBox.item(i - 1).style.display = "block";
-        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-    }
-    if (i % 2 == 0) {
-        document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
-        document.getElementById("contact-link").style.color = "#000";
-        for (let j = 0; j < 5; j++) {
-            document.getElementsByClassName("hover")[j].style.color = "#000";
-        }
+// function right(i) {
+//     var linkBox = document.getElementsByClassName("section-first-link");
+//     if (i < 6 && i != 1) {
+//         linkBox.item(i - 2).style.display = "none";
+//         linkBox.item(i - 1).style.display = "block";
+//         document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+//     } else if (i == 1) {
+//         linkBox.item(4).style.display = "none";
+//         linkBox.item(i - 1).style.display = "block";
+//         document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+//     }
+//     if (i % 2 == 0) {
+//         document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
+//         document.getElementById("contact-link").style.color = "#000";
+//         for (let j = 0; j < 5; j++) {
+//             document.getElementsByClassName("hover")[j].style.color = "#000";
+//         }
 
-    } else {
-        document.getElementById("logo").src = "/assets/img/logo_hell.png";
-        document.getElementById("contact-link").style.color = "#fafafa";
-        for (let j = 0; j < 5; j++) {
-            document.getElementsByClassName("hover")[j].style.color = "#fafafa";
-        }
-    }
-}
-function left(i) {
-    var linkBox = document.getElementsByClassName("section-first-link");
-    if (i > 0 && i != 5) {
-        linkBox.item(i).style.display = "none";
-        linkBox.item(i - 1).style.display = "block";
-        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-    } else if (i == 5) {
-        linkBox.item(0).style.display = "none";
-        linkBox.item(4).style.display = "block";
-        document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
-    }
-    if (i % 2 == 0) {
-        document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
-        document.getElementById("contact-link").style.color = "#000";
-        for (let j = 0; j < 5; j++) {
-            document.getElementsByClassName("hover")[j].style.color = "#000";
-        }
-    } else {
-        document.getElementById("logo").src = "/assets/img/logo_hell.png";
-        document.getElementById("contact-link").style.color = "#fafafa";
-        for (let j = 0; j < 5; j++) {
-            document.getElementsByClassName("hover")[j].style.color = "#fafafa";
-        }
-    }
-}
+//     } else {
+//         document.getElementById("logo").src = "/assets/img/logo_hell.png";
+//         document.getElementById("contact-link").style.color = "#fafafa";
+//         for (let j = 0; j < 5; j++) {
+//             document.getElementsByClassName("hover")[j].style.color = "#fafafa";
+//         }
+//     }
+// }
+// function left(i) {
+//     var linkBox = document.getElementsByClassName("section-first-link");
+//     if (i > 0 && i != 5) {
+//         linkBox.item(i).style.display = "none";
+//         linkBox.item(i - 1).style.display = "block";
+//         document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+//     } else if (i == 5) {
+//         linkBox.item(0).style.display = "none";
+//         linkBox.item(4).style.display = "block";
+//         document.getElementById("section-first").style.backgroundImage = `url('/assets/img/menu_${i}.png')`;
+//     }
+//     if (i % 2 == 0) {
+//         document.getElementById("logo").src = "/assets/img/logo_dunkel.png";
+//         document.getElementById("contact-link").style.color = "#000";
+//         for (let j = 0; j < 5; j++) {
+//             document.getElementsByClassName("hover")[j].style.color = "#000";
+//         }
+//     } else {
+//         document.getElementById("logo").src = "/assets/img/logo_hell.png";
+//         document.getElementById("contact-link").style.color = "#fafafa";
+//         for (let j = 0; j < 5; j++) {
+//             document.getElementsByClassName("hover")[j].style.color = "#fafafa";
+//         }
+//     }
+// }
 function grid(i) {
     var linkBox = document.getElementsByClassName("section-first-link");
     if (i == 0) {
@@ -74,26 +74,26 @@ function grid(i) {
 // ------------------------------ CODE VON JASCHA ------------------------------
 function rightOfSectionThree(i) {
     var linkBoxOfSectionThree = document.getElementsByClassName("section-three-link");
-    if (i < 6 && i != 1) {
-        linkBoxOfSectionThree.item(i - 2).style.display = "none";
-        linkBoxOfSectionThree.item(i - 1).style.display = "block";
-    }
-    else if (i == 1) {
-        linkBoxOfSectionThree.item(4).style.display = "none";
-        linkBoxOfSectionThree.item(i - 1).style.display = "block";
-        document.getElementById("ClassOfPictureSliderPicture").style.backgroundImage = `url('/assets/img/produkt${i}.png')`;
+    if (i == 1) {
+        linkBoxOfSectionThree.item(2).style.display = "none";
+        linkBoxOfSectionThree.item(0).style.display = "block";
+    } else if (i == 2) {
+        linkBoxOfSectionThree.item(0).style.display = "none";
+        linkBoxOfSectionThree.item(1).style.display = "block";
+    } else {
+        linkBoxOfSectionThree.item(1).style.display = "none";
+        linkBoxOfSectionThree.item(2).style.display = "block";
     }
 }
 
 function leftOfSectionThree(i) {
     var linkBoxOfSectionThree = document.getElementsByClassName("section-three-link");
-    if (i > 0 && i != 5) {
+    if (i > 0 && i != 3) {
         linkBoxOfSectionThree.item(i).style.display = "none";
         linkBoxOfSectionThree.item(i - 1).style.display = "block";
-    } else if (i == 5) {
+    } else if (i == 3) {
         linkBoxOfSectionThree.item(0).style.display = "none";
-        linkBoxOfSectionThree.item(4).style.display = "block";
-        document.getElementById("ClassOfPictureSliderPicture").style.backgroundImage = `url('/assets/img/produkt${i}.png')`;
+        linkBoxOfSectionThree.item(2).style.display = "block";
     }
 }
 
